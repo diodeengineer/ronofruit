e38ef4fec9483cf3dc1fdfe7d23176c58624ebf7
Diode Lab <deelepboidya@gmail.com>
2025-07-01 17:53:14 +0530

    add location

---
+++ src/app/components/locationmap/LocationMap.js
@@ -0,0 +1,23 @@
+
+import styles from "./LocationMap.module.css";
+
+export default function LocationMap() {
+  return (
+    <section className={styles.mapSection}>
+      <h2 className={styles.heading}>Visit Us</h2>
+      <div className={styles.mapContainer}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d220.87253220956512!2d94.7053194402294!3d27.656693302808513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1751372299350!5m2!1sen!2sin"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <p className={styles.address}>
+          Rono Fruit Stall, Opposite Jainath Store, Likabali, Arunachal Pradesh, India
+        </p>
+      </div>
+    </section>
+  );
+}
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,20 @@
 
 
 import HeroSection from "./components/herosection/HeroSection";
+import LocationMap from "./components/locationmap/LocationMap";
 import ProductGrid from "./components/productgrid/ProductGrid";
+import Testimonials from "./components/testimonial/Testimonial";
 import styles from "./page.module.css";
+
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <main className={styles.main}>
-        <div className={styles.ctas}>
-          {/* You can add calls to action or other sections here */}
-           <ProductGrid />
-        </div>
+        <ProductGrid />
+        <Testimonials />
+        <LocationMap />
       </main>
     </>
   );
