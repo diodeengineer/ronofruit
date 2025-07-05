4771183fc76d2fb72af5a05696a70adaf6129bf8
Diode Lab <deelepboidya@gmail.com>
2025-07-05 12:56:35 +0530

    add map

--- src/app/components/locationmap/LocationMap.js
+++ src/app/components/locationmap/LocationMap.js
@@ -1,4 +1,5 @@
 
+
 import styles from "./LocationMap.module.css";
 
 export default function LocationMap() {
@@ -6,7 +7,7 @@ export default function LocationMap() {
     <section className={styles.mapSection}>
       <h2 className={styles.heading}>Visit Us</h2>
       <div className={styles.mapContainer}>
-        <iframe
+        <iframe className={styles.iframe}
           src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d220.87253220956512!2d94.7053194402294!3d27.656693302808513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1751372299350!5m2!1sen!2sin"
           style={{ border: 0 }}
           allowFullScreen
@@ -16,6 +17,14 @@ export default function LocationMap() {
         <p className={styles.address}>
           Rono Fruit Stall, Opposite Jainath Store, Likabali, Arunachal Pradesh, India
         </p>
+        <a
+          href="https://www.google.com/maps/place/Rono+Fruit+Stall,+Likabali"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.directionsButton}
+        >
+          Get Directions
+        </a>
       </div>
     </section>
   );
